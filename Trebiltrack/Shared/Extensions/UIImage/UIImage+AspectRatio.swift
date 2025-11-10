3e64246fcc3c5d3cc9affed365ee3128cf1228a9
sebjvidal <seb.vidal@btinternet.com>
2025-11-10 16:11:15 +0000

    Add `UIImage` extensions

---
+++ Trebiltrack/Shared/Extensions/UIImage/UIImage+AspectRatio.swift
@@ -0,0 +1,14 @@
+//
+//  UIImage+AspectRatio.swift
+//  Trebiltrack
+//
+//  Created by Seb Vidal on 10/11/2025.
+//
+
+import UIKit
+
+extension UIImage {
+    var aspectRatio: CGFloat {
+        return size.aspectRatio
+    }
+}
