a4640e657e054d94870d9a72ef3606b396b10f9b
sebjvidal <seb.vidal@btinternet.com>
2025-11-10 16:12:06 +0000

    Add `MKMapConfiguration` extensions

---
+++ Trebiltrack/Shared/Extensions/MKMapConfiguration/MKMapConfiguration+Constants.swift
@@ -0,0 +1,12 @@
+//
+//  MKMapConfiguration+Constants.swift
+//  Trebiltrack
+//
+//  Created by Seb Vidal on 10/11/2025.
+//
+
+import MapKit
+
+extension MKMapConfiguration {
+    static let realisticImagery = MKImageryMapConfiguration(elevationStyle: .realistic)
+}
